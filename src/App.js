59c11aa79730d694f56d9621a80d3b5ce791b60f
AlfredSkillstorm <aabenojar@skillstorm.com>
2021-10-26 14:54:02 -0500

    working form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import { Form } from './components/Form/Form';
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <Form>
+        {/* Put all elements here */}
+      </Form>
     </div>
   );
 }
