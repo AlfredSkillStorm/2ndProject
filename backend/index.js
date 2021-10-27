84df882ef93a008d923fe44d46c87d424f5cb1b7
AlfredSkillstorm <aabenojar@skillstorm.com>
2021-10-27 18:08:24 -0500

    Added functionality

--- backend/index.js
+++ backend/index.js
@@ -9,10 +9,14 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
 
-app.use('/movies', require('./routes/movie'));
+//app.use('/movies', require('./routes/movie'));
 
 app.get('/', (req, res) => {
     
+});
+
+app.get('/helloThere', (req, res) => {
+    console.log('Hello there >:3');
 });
 
 app.listen(port, () => {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,29 @@
 import Field from "./components/Field";
 import { Form } from './components/Form/Form';
 import Header from './components/Header';
+import axios from 'axios';
+import {useState, useEffect} from 'react';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 
 function App() {
+  const [hotels, setHotels] = useState([]);
   return (
-    <div className="App">
-      <Form>
-        <Field text='User Name: '/>
-        <Field text='Password: '/>
-        <Header/>
-      </Form>
-    </div>
+    <Router>
+      
+      <Switch>
+        <Route path='/' exact>
+          <div className="App">
+          <Form>
+          <Field text='User Name: '/>
+          <Field text='Password: '/>
+          <Header/>
+          </Form>
+          </div>
+        </Route>
+        <Route path='/helloThere'> </Route>
+      </Switch>
+    </Router>
+    
   );
 }
 
